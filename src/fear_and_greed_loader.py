ccb5518f2811e03f8ec7929b3b98cdde19e45214
linuskemper <linus_kemper@icloud.com>
2025-12-05 17:11:40 +0100

    Created base for fear and greed

---
+++ src/fear_and_greed_loader.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+API_URL = "https://api.alternative.me/fng/"
+START_DATE = datetime(2020, 1, 1).date()
+END_DATE = datetime(2024, 12, 31).date()
+OUTPUT_FILE = "data/fear_greed_2022_2024.csv"
